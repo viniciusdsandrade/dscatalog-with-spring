8f02068ec6770719d4ef60c9f06ce6a35db6f479
andrade <viniciusdsandrade0662@gmail.com>
2025-10-13 00:01:37 -0300

    feat: add SQL query to retrieve products with their associated categories

--- db_ds_catalog.sql
+++ db_ds_catalog.sql
@@ -8,3 +8,12 @@ SHOW FULL TABLES FROM db_ds_catalog;
 DESCRIBE tb_category;
 DESCRIBE tb_product;
 DESCRIBE tb_product_category;
+
+
+SELECT p.name                                                       AS produto,
+       GROUP_CONCAT(DISTINCT c.name ORDER BY c.name SEPARATOR ', ') AS categorias
+FROM tb_product_category pc
+         JOIN tb_product p ON p.id = pc.product_id
+         JOIN tb_category c ON c.id = pc.category_id
+GROUP BY p.id, p.name
+ORDER BY p.name;
