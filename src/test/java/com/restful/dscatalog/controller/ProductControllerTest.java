7489a35627c8303bddcfbf1076e86494bde430c0
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-10-17 16:37:24 -0300

    test: replace MockBean with MockitoBean in ProductControllerTest

--- src/test/java/com/restful/dscatalog/controller/ProductControllerTest.java
+++ src/test/java/com/restful/dscatalog/controller/ProductControllerTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -41,7 +41,7 @@ class ProductControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private ProductService productService;
 
     private String baseUrl;
