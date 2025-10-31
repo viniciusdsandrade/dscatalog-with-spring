883e6f730c1eff62a3ffd9f81ea2d377391aef57
andrade <viniciusdsandrade0662@gmail.com>
2025-10-31 14:17:23 -0300

    refactor: change DatabaseException response status from BAD_REQUEST to CONFLICT

--- src/main/java/com/restful/dscatalog/exception/DatabaseException.java
+++ src/main/java/com/restful/dscatalog/exception/DatabaseException.java
@@ -2,9 +2,9 @@ package com.restful.dscatalog.exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
-@ResponseStatus(BAD_REQUEST)
+@ResponseStatus(CONFLICT)
 public class DatabaseException extends RuntimeException {
     public DatabaseException(String message) {
         super(message);
